21100a9647d209863ce601022c059f175a5c66af
KareemSawy <kareemkhaledelsawy@gmail.com>
2026-03-02 00:10:48 +0200

    Delete src directory

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "https://limelime.vercel.app";
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/admin", "/api", "/auth"] }],
-    sitemap: `${baseUrl}/sitemap.xml`,
-  };
-}
